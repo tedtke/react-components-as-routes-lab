49576e8e9349fb736408126ba5deac78aa38cfa5
Teodoro Garcia <tedtke514@gmail.com>
2021-03-31 21:53:55 -0500

    commit tree ebf52fe57ca8e6071fe394832b4a7d3431f04ee4

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -4,9 +4,21 @@ import { directors } from '../data';
 const Directors = () => {
   return (
     <div>
-      {/*{code here}*/}
+     <h1>Directors Page</h1>
+      {directors.map((director, index) => (
+          <div key={index}>
+              <h3>Name: {director.name}</h3>
+              <p>Movies:</p>
+              <ul>
+                  {director.movies.map((movie, index) => (
+                      <li key={index}>{movie}</li>
+                  ))}
+              </ul>
+          </div>
+      ))}
     </div>
   );
 }
 
 export default Directors
+// This component should render the text Directors Page in an <h1>, and make a new <div> for each director. The <div> should contain the director's name and a <ul> with a list of their movies.
